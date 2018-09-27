c8862a521d932b107aa15b6710140952c5856adc
liumapp <liumapp.com@gmail.com>
2018-09-27 14:09:04 +0800

    add doc 2 html class

---
+++ src/main/java/com/liumapp/convert/html/fromdoc/Doc2Html.java
@@ -0,0 +1,15 @@
+package com.liumapp.convert.html.fromdoc;
+
+/**
+ * file Doc2Html.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/27
+ */
+public class Doc2Html {
+
+
+
+}
