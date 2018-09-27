aeb49998c54370d7b61e538554039a0615fc4f53
liumapp <liumapp.com@gmail.com>
2018-09-27 15:15:00 +0800

    ready to test

---
+++ src/test/java/com/liumapp/convert/html/fromdoc/Doc2HtmlTest.java
@@ -0,0 +1,20 @@
+package com.liumapp.convert.html.fromdoc;
+
+import org.junit.Test;
+
+/**
+ * file Doc2HtmlTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/27
+ */
+public class Doc2HtmlTest {
+
+    @Test
+    public void testConvert () {
+        
+    }
+
+}
